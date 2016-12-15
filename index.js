2abddbb20523d5ea4095fbd89bfa86f83f0632d1
Nealcn <cuiyu_tea@163.com>
2016-12-15 16:55:19 +0800

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function(req,res){
-	res.writeHead(200,{'Content-Type': 'text/plain'});
-	res.end('Hello Word --Neal\n');
-}).listen(80,'https://neal.mybluemix.net');
-console.log('Server running at https://neal.mybluemix.net:80');
