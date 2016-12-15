26b5925631815c25117ed8217aff4a3fd6304c6a
Nealcn <cuiyu_tea@163.com>
2016-12-15 11:42:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-<html>
-     <body>
-          Hello, Bluemix World!
-     </body>
-</html>
+var http = require('http');
+http.createServer(function(req,res){
+	res.writeHead(200,{'Content-Type': 'text/plain'});
+	res.end('Hello Word --Neal\n');
+}).listen(80,'https://neal.mybluemix.net');
+console.log('Server running at https://neal.mybluemix.net:80');
